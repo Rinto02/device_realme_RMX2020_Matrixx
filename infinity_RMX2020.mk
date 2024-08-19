b11ec31fad312e0331541ab37c7b5a5efc8d4a7b
凛岡村リント <138510229+Rinto02@users.noreply.github.com>
2024-08-19 15:11:27 +0600

    RMX2020: Missing Infinity Flags

--- infinity_RMX2020.mk
+++ infinity_RMX2020.mk
@@ -12,8 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile
 $(call inherit-product, device/realme/RMX2020/device.mk)
 
-# Inherit some common Lineage stuff.
-TARGET_DISABLE_EPPE := true
+# Inherit some common Infinity stuff.
 $(call inherit-product, vendor/infinity/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
@@ -31,12 +30,14 @@ PRODUCT_SYSTEM_DEVICE := RMX2020
 TARGET_BOOT_ANIMATION_RES := 720
 
 # Infinity Stuffs
+INFINITY_BUILD_TYPE := UNOFFICIAL
 INFINITY_MAINTAINER := Rinto
 TARGET_ENABLE_BLUR := true
 WITH_GAPPS := false
 TARGET_BUILD_VIMUSIC := true
 USE_MOTO_CALCULATOR := true
 TARGET_SUPPORTS_QUICK_TAP := false
+TARGET_CALL_RECORDING_SUPPORTED := true
 
 # Build info
 BUILD_FINGERPRINT := "realme/RMX2020/RMX2020:11/RP1A.200720.011/1652239672541:user/release-keys"
