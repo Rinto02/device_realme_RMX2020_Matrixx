3ce2fdfdd9543e10abd8b2a3c9fe08846b6496a7
Sarthak Roy <sarthakroy2002@gmail.com>
2024-09-18 23:29:19 +0530

    RMX2020: Setup FP/Non-FP Variant SKU
    * Inspired from RM6785 unifications
    
    Signed-off-by: Sarthak Roy <sarthakroy2002@gmail.com>
    Change-Id: Iefdf18a872a51118b2970572202ced0babce54ad

--- device.mk
+++ device.mk
@@ -352,9 +352,11 @@ PRODUCT_PACKAGES += \
     ApertureOverlayRMX2020 \
     ApertureQRScannerOverlayRMX2020 \
     CarrierConfigOverlayRMX2020 \
+    FingerprintResOverlayRMX2020 \
     FrameworksResOverlayRMX2020 \
     SettingsOverlayRMX2020 \
     SettingsProviderOverlayRMX2020 \
+    SettingsProviderOverlayRMX2027 \
     SystemUIOverlayRMX2020 \
     TelephonyOverlayRMX2020 \
     TetheringResOverlayRMX2020 \
